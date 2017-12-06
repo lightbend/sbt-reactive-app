f133f50240acae5d674086b2a18526aaf0d33b3d
Jason Longshore <longshorej@gmail.com>
2017-12-06 08:27:55 -0600

    Bump reactive-lib to 0.2.2

--- src/main/scala/com/lightbend/rp/sbtreactiveapp/App.scala
+++ src/main/scala/com/lightbend/rp/sbtreactiveapp/App.scala
@@ -56,7 +56,7 @@ sealed trait App extends SbtReactiveAppKeys {
     environmentVariables := Map.empty,
     startScriptLocation := Some("/rp-start"),
     secrets := Set.empty,
-    reactiveLibVersion := "0.2.1",
+    reactiveLibVersion := "0.2.2",
     reactiveLibAkkaClusterBootstrapProject := "reactive-lib-akka-cluster-bootstrap" -> true,
     reactiveLibCommonProject := "reactive-lib-common" -> true,
     reactiveLibPlayHttpBindingProject := "reactive-lib-play-http-binding" -> true,
